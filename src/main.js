9e3b0a5dc0a4040fb5a64f2f4a47d2795ec1ea8e
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-11-29 08:23:02 -0500

    Adding Vuetify + Firebase

--- src/main.js
+++ src/main.js
@@ -6,11 +6,16 @@ import store from './store'
 import Vuetify from 'vuetify'
 Vue.use(Vuetify)
 import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
